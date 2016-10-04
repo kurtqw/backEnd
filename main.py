3cea2046545126812a022b2808ab0c701804078c
wqlin <wqlin0@gmail.com>
2016-10-04 14:12:41 +0800

    添加获取热点新闻功能

--- main.py
+++ main.py
@@ -12,22 +12,25 @@ import tornado.ioloop
 import tornado.options
 from uuid import uuid4
 from queue import Queue
-
+from newsHandler import NewsHandler
 
 chattingList = {}
 
+
 class Chat(object):
-    def __init__(self,id1,id2):
+    def __init__(self, id1, id2):
         self.id1 = id1
         self.id2 = id2
-        self.isClose = False #是否有人退出
-    def register(self,id,handler):
+        self.isClose = False  # 是否有人退出
+
+    def register(self, id, handler):
         if id == self.id1:
             self.handler1 = handler
         elif id == self.id2:
             self.handler2 = handler
         else:
             print("Error")
+
     def recMessage(self, data):
         if data.get('id') == self.id1:
             self.handler2.write_message(data.get('text'))
@@ -51,14 +54,14 @@ class Chat(object):
             chattingList[id].handler1.close()
 
 
-
 class Waiter(object):
     '''
     TODO : 保存聊天记录
     '''
     waiting = Queue()
     callbacks = Queue()
-    def match(self,id,callback):
+
+    def match(self, id, callback):
         '''
 
         :param id:
@@ -67,49 +70,50 @@ class Waiter(object):
         '''
         self.waiting.put(id)
         self.callbacks.put(callback)
-        if  self.waiting.qsize() > 1:
+        if self.waiting.qsize() > 1:
             self.notify()
+
     def notify(self):
         id1 = self.waiting.get()
         id2 = self.waiting.get()
-        print(id1,id2)
+        print(id1, id2)
         callback1 = self.callbacks.get()
         callback2 = self.callbacks.get()
         callback1(id1)
         callback2(id2)
-        chat = Chat(id1,id2)
+        chat = Chat(id1, id2)
         chattingList[id1] = chat
         chattingList[id2] = chat
 
 
-
 class MainHandler(tornado.web.RequestHandler):
     idSet = set()
+
     @tornado.web.asynchronous
     def get(self):
         id = str(uuid4())
         while id in self.idSet:
             id = str(uuid4())
         self.idSet.add(id)
-        self.application.waiter.match(id,self.returnId)
+        self.application.waiter.match(id, self.returnId)
+
     def set_default_headers(self):
-        #跨域
+        # 跨域
         self.set_header('Access-Control-Allow-Origin', "null")
         self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
         self.set_header('Access-Control-Max-Age', 1000)
         self.set_header('Access-Control-Allow-Headers', '*')
-        self.set_header('Access-Control-Allow-Credentials',"true")
+        self.set_header('Access-Control-Allow-Credentials', "true")
 
     def returnId(self, id):
-        self.write(json.dumps({'status':0,'id':id}))#
+        self.write(json.dumps({'status': 0, 'id': id}))  #
         self.finish()
-
 
 
 class ChatHandler(tornado.websocket.WebSocketHandler):
     def open(self):
-        self.id = self.get_argument("id",-1)
-        chattingList[self.id].register(self.id,self)
+        self.id = self.get_argument("id", -1)
+        chattingList[self.id].register(self.id, self)
 
     def on_message(self, data):
         chattingList[self.id].recMessage(json.loads(data))
@@ -121,6 +125,7 @@ class ChatHandler(tornado.websocket.WebSocketHandler):
             chattingList[self.id].notifyClose(self.id)
         else:
             print("另一方也退出")
+
     def check_origin(self, origin):
         return True
 
@@ -129,10 +134,12 @@ class Application(tornado.web.Application):
     def __init__(self):
         self.waiter = Waiter()
         handlers = [
-            (r"/",MainHandler),
-            (r"/chat",ChatHandler)
+            (r"/", MainHandler),
+            (r"/chat", ChatHandler),
+            (r"/news", NewsHandler)
         ]
-        super(Application,self).__init__(handlers)
+        super(Application, self).__init__(handlers)
+
 
 if __name__ == '__main__':
     tornado.options.parse_command_line()
---
+++ newsHandler.py
@@ -0,0 +1,50 @@
+# coding=utf-8
+import tornado.web
+import pymysql
+import json
+import configparser
+
+
+class NewsHandler(tornado.web.RequestHandler):
+    def __init__(self, application, request, **kargs):
+        parser = configparser.ConfigParser()
+        parser.read('mysql.ini')
+        host = parser['CONFIG']['HOST']
+        username = parser['CONFIG']['USERNAME']
+        password = parser['CONFIG']['PASSWORD']
+        db = parser['CONFIG']['DB']
+
+        self.conn = pymysql.connect(host, username, password, db,charset='utf8')
+        self.cur = self.conn.cursor()
+        self.select_stmt = 'select * from news order by pv desc'
+        self.size = self.cur.execute(self.select_stmt)
+        l = list(range(0, self.size, 9))
+        self.index = list(zip(l, l[1:]))
+        self.res = self.cur.fetchall()
+        super(NewsHandler, self).__init__(application, request, **kargs)
+
+    def get(self):
+        page = int(self.get_argument("page", 1))
+        try:
+            head = self.index[page - 1][0]
+            tail = self.index[page - 1][1]
+            newsTuple = self.res[head:tail]
+            data = []
+            for t in newsTuple:
+                news = {}
+                news['id'] = t[0]
+                news['url'] = t[1]
+                news['title'] = t[2]
+                news['pv'] = t[3]
+                data.append(news)
+            self.write(json.dumps({'status': 0, 'data': data}))
+        except IndexError as e:
+            self.write(json.dumps({'status': 1, 'data': 'page exceed limits'}))
+
+        '''
+        TODO 更新热点的pv数
+        '''
+
+# if __name__ == '__main__':
+#    n = NewsHandler()
+#    n.get()
