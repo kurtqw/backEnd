6a3a4a33de88efe2ced3981bece02b874106b310
wqlin <wqlin0@gmail.com>
2016-10-05 19:25:59 +0800

    change table news design

--- townmeet.sql
+++ townmeet.sql
@@ -4,7 +4,6 @@ CREATE TABLE news (
   `id`       INT          NOT NULL AUTO_INCREMENT,
   `url`      VARCHAR(50)  NOT NULL,
   `title` text NOT NULL,
-  `type`     VARCHAR(200) NOT NULL,
-  `pv`       INT                   DEFAULT 0,
+  `visit_cnt`       INT                   DEFAULT 0,
   PRIMARY KEY (id)
 );
